beaf0e01480a86293bb749579e38283b9d267494
Andrea Calabretta <andreacalabretta0@gmail.com>
2025-05-16 18:08:06 +0200

    feat: viewchild button autoclicked

---
+++ src/app/components/viewchild/viewchild.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-viewchild',
+  standalone: false,
+  templateUrl: './viewchild.component.html',
+  styleUrl: './viewchild.component.scss'
+})
+export class ViewchildComponent implements AfterViewInit, OnDestroy{
+   @ViewChild('autoButton') buttonRef!: ElementRef<HTMLButtonElement>;
+  clickCount = 0;
+  private intervalId: any;
+
+  ngAfterViewInit(): void {
+    // Ogni 5 secondi, fai click programmatico
+    this.intervalId = setInterval(() => {
+      this.buttonRef.nativeElement.click();
+    }, 5000);
+  }
+
+  onAutoClick(): void {
+    this.clickCount++;
+    console.log('Button clicked!', this.clickCount);
+  }
+
+  ngOnDestroy(): void {
+    // Pulizia importante
+    clearInterval(this.intervalId);
+  }
+
+}
